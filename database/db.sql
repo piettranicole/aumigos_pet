5fe7eb4453e4be19c5a181f0d8c68698088174ca
piettra <piettra_cidral@estudante.sesisenai.org.br>
2026-08-25 11:16:42 -0300

    add- db.sql

---
+++ database/db.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE aumigos_pet;
+
+USE aumigos_pet;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE pets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    especie VARCHAR(50) NOT NULL,
+    raca VARCHAR(50) NOT NULL,
+    idade INT NOT NULL,
+    
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
